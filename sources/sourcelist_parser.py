15603d8c314baa55c27ee0ab1a5becb67585161c
Peter Wolf <pwolf2310@gmail.com>
2016-03-27 23:35:27 +0200

    Comments in sources.list will be ignored

--- sources/sourcelist_parser.py
+++ sources/sourcelist_parser.py
@@ -2,19 +2,25 @@
 
 import os.path
 from source import Source
+import logging
 
 def parse_sources(list_file):
     sources = []
+    logging.debug(list_file)
     if os.path.exists(list_file):
-		with open(list_file, "r") as sources_file:
-			lines = sources_file.readlines()
-			sources = [parse_source(line) for line in lines]
-    
+        with open(list_file, "r") as sources_file:
+            lines = sources_file.readlines()
+            for line in lines:
+                logging.debug(line.strip())
+                if line.startswith("#") or line.startswith("//") or line.strip() == "":
+                    continue
+                sources.append(parse_source(line))
     return sources
 
 
 def parse_source(line):
-	tokens = line.split()
-	source_type = tokens[0]
-	id = tokens[1]
-	return Source(source_type, id)
+    logging.debug(line)
+    tokens = line.split()
+    source_type = tokens[0]
+    id = tokens[1]
+    return Source(source_type, id)
